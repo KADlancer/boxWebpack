0efe150f2ab8ea4a7a6890022f41102ba642108c
Konrad Abe <konrad.abe@interactive-tools.de>
2018-07-09 00:20:59 +0200

    Added Babel Loader and switched login.js to login.es6, using es6 features

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,17 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.es6$/i,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.es6']
     }
 };
