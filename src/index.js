7f36245cb328c9484e03dc10ae6c801a74a66735
Konrad Abe <konrad.abe@interactive-tools.de>
2018-07-08 23:33:47 +0200

    added login and utils js files and used both require (login) and the config file entry points (utils)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 
+require('./login');
+
 function component() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello, oh my', 'webpack'], ' ');
 
     return element;
 }
