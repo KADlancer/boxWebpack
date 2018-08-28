6100649196b3a49a396719dddd35af3e91aa00f5
Konrad Abe <mail@konrad-abe.de>
2018-08-28 21:06:40 +0200

    added another small demo react script for testing

---
+++ src/Demo.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        };
+    }
+    render() {
+        const { counter } = this.state;
+        return (
+            <div>
+                <div>{counter}</div>
+                <div>{counter === 0 ? 'zero' : counter.toString()}</div>
+                <button
+                    onClick={() => this.setState({ counter: counter + 1 })}
+                >+</button>
+            </div>
+        );
+    }
+}
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom'
 import './index.css';
 import './css/app.css';
 
-import App from './App'
+import App from './App';
+import Demo from './Demo';
 
 
 require('./js/login');
 
 require('./js/demo/tictactoe/game');
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('demo'));
