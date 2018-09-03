8d55661b2378fcaa9a1b0026f40609572f2f560c
Konrad Abe <mail@konrad-abe.de>
2018-09-03 17:35:05 +0200

    added storybook addon-info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure, addDecorator } from '@storybook/react'
+import { withInfo, setDefaults } from '@storybook/addon-info'
 
+import './style.css'
 
 
 const req = require.context('../src/', true, /stories\.js$/);
@@ -7,4 +9,9 @@ function loadStories() {
 	req.keys().forEach(req)
 }
 
+setDefaults({
+  inline: true,
+})
+
+addDecorator((story, context) => withInfo(context.kind)(story)(context))
 configure(loadStories, module);
