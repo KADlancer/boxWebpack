518175087aeed9ccea03ef8794ea40224f0021ab
Konrad Abe <mail@konrad-abe.de>
2018-09-07 23:40:38 +0200

    added "why did you update" react re-render check (experimental)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import Dashboard from "./components/pages/dashboard/Dashboard";
 
+if (process.env.NODE_ENV !== 'production') {
+	const {whyDidYouUpdate} = require('why-did-you-update');
+	whyDidYouUpdate(React);
+}
+
 ReactDOM.render(
 	<Dashboard />,
 	document.getElementById("root")
