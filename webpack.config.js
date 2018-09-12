2f170ba3c17d64f1188f546958d86b064b8468e0
Konrad Abe <mail@konrad-abe.de>
2018-09-12 21:47:13 +0200

    Code Cleanup #8.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
 
 module.exports = merge(baseConfig, {
 	mode: 'development',
-	entry: "./src/index.js",
+	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, "dist/"),
-		publicPath: "/dist/",
-		filename: "bundle.js"
+		path: path.resolve(__dirname, 'dist/'),
+		publicPath: '/dist/',
+		filename: 'bundle.js'
 	},
 	devServer: {
-		contentBase: path.join(__dirname, "public/"),
+		contentBase: path.join(__dirname, 'public/'),
 		port: 3000,
-		publicPath: "http://localhost:3000/dist/",
+		publicPath: 'http://localhost:3000/dist/',
 		hotOnly: true
 	},
 	plugins: [ new webpack.HotModuleReplacementPlugin() ]
