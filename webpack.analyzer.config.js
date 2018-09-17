502c04ebe30178be1b2204c667f5117951ab6abf
Konrad Abe <mail@konrad-abe.de>
2018-09-17 12:42:32 +0200

    added webpack analyzer

---
+++ webpack.analyzer.config.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.production.config');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(baseConfig, {
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            reportFilename: "report.html",
+        })
+    ],
+});
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -14,5 +14,5 @@ module.exports = merge(baseConfig, {
 		rules: [
 			stripLoader
 		]
-	}
+	},
 });
